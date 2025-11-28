dfce49a1ca544bf8c69a6ce795308b8cade3b03f
Sebastian Gomez <sebasgomez2923@gmail.com>
2025-11-28 18:22:57 -0500

    Se utiliza react - hook- form para la validacion del formulario de registro

--- frontend/src/router.tsx
+++ frontend/src/router.tsx
@@ -1,20 +1,17 @@
 import { BrowserRouter, Routes, Route} from 'react-router-dom'
 import LoginView from './views/LoginView'
 import RegisterView from './views/RegisterView'
+import AuthLayout from './Layout/AuthLayout'
 
 export default function Router() {
     return (
         <BrowserRouter>
             <Routes>
-                <Route>
+                <Route element={<AuthLayout/>}>
                     <Route path='/auth/login' element={<LoginView />} />
-                </Route>
-
-                <Route>
                     <Route path='/auth/register' element={<RegisterView />} />
+
                 </Route>
-
-
             </Routes>
         </BrowserRouter>
     )
--- frontend/src/views/LoginView.tsx
+++ frontend/src/views/LoginView.tsx
@@ -1,5 +1,14 @@
+import {Link} from 'react-router-dom'
+
 export default function LoginView() {
     return (
-        <div className="text-5xl">LoginView</div>
+        <>            
+        <h1 className=' text-4xl text-white font-bold'>Iniciar Sesion</h1>
+
+        <nav className=' mt-7'> 
+                {/*Se utiliza Link para navegar de manera interna en la aplicacion, y se debe utilizar la etiqueda <a></a> para paginas externas*/}
+                <Link className='text-center text-white text-lg block' to="/auth/register"> ¿No tienes cuenta? Crea una aqui</Link>
+            </nav>
+        </>
     )
 }
